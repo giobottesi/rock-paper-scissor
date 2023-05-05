ec83f06a1c8bcdd4735d79c90e1bea4864e4e8f6
Giovanna Bottesi <giovannabottesi@gmail.com>
2023-05-05 16:20:49 -0300

    Create game function

--- script.js
+++ script.js
@@ -5,22 +5,46 @@ function getComputerChoice (options) {
 }
 
 const playerSelection = "rock";
-const computerSelection = getComputerChoice(gameOptions);
 
 function playRound(playerSelection, computerSelection) {
   lowercasedPlayerSelection = playerSelection.toLowerCase()
 
   if(playerSelection.toLowerCase() === computerSelection) {
-    return "It's a tie!";
+    return "tie";
   } else if(playerSelection.toLowerCase() === "rock") {
-    return computerSelection === "paper" ? "You Lose! Paper beats Rock" : "You Won! Rock beats Scissor";
+    return computerSelection === "paper" ? "computer" : "player";
   } else if(playerSelection.toLowerCase() === "paper") {
-    return computerSelection === "scissor" ? "You Lose! Scissor beats Paper" : "You Won! Paper beats Rock";
+    return computerSelection === "scissor" ? "computer" : "player";
   } else if(playerSelection.toLowerCase() === "scissor") {
-    return computerSelection === "rock" ? "You Lose! Rock beats Scissor" : "You Won! Scissor beats Paper";
+    return computerSelection === "rock" ? "computer" : "player";
   }
 }
 
-console.log(playRound(playerSelection, computerSelection))
-console.log("Player: " + playerSelection)
-console.log("Computer: " + computerSelection)
+function game () {
+  playerScore = 0
+  computerScore = 0
+  for (let i = 1; i <= 5; i++ ) {
+    let playerSelection = prompt("Choose between rock, paper or scissor!!")
+    let computerSelection = getComputerChoice(gameOptions);
+
+    result = playRound(playerSelection, computerSelection)
+
+    if (result === "player") {
+      message = "You won! " + playerSelection + " beats " + computerSelection
+      playerScore++
+    } else if (result === "computer") {
+      message = "You lose! " + computerSelection + " beats " + playerSelection
+      computerScore++
+    } else {
+      message = "It's a tie!"
+    }
+
+    console.log(message)
+    console.log("Player score: " + playerScore)
+    console.log("Computer score: " + computerScore)
+  }
+
+  playerScore > computerScore ? console.log("Congrats! You won!") : console.log("Oh no! You lose.")
+}
+
+console.log(game())
